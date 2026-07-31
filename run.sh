811a0e00632045dca9d129e350058d371985219a
Tim Aerts <aardbeiplantje@gmail.com>
2026-07-31 12:14:27 +0000

    pass env vars through to nginx and container: MODEL, MODEL_PATH, HF_HOME, LLAMA_PRESETS, ROCm vars

--- run.sh
+++ run.sh
@@ -67,10 +67,24 @@ exec docker run \
     $([ -n "$LLAMA_PRESETS" ] && echo "-v $LLAMA_PRESETS:/llama/llamacpp_presets.ini") \
     -v llama.cpp-data:/llama.cpp:rw \
     $([ -n "$HF_HOME" ] && echo "-v $HF_HOME:/hf:rw") \
-    -e XDG_CACHE_HOME=/dev/shm \
-    -e HF_HOME=/hf \
-    -e HF_TOKEN \
-    -e HF_HUB_CACHE=/hf/hub \
+    -e XDG_CACHE_HOME=${XDG_CACHE_HOME:-/dev/shm} \
+    -e HF_HOME=${HF_HOME:-/hf} \
+    -e HF_TOKEN=${HF_TOKEN:-} \
+    -e HF_HUB_CACHE=${HF_HUB_CACHE:-/hf/hub} \
+    -e MODEL=${MODEL:-} \
+    -e MODEL_PATH=${MODEL_PATH:-/models} \
+    -e MODELS_DIR=${MODELS_DIR:-} \
+    -e LLAMA_PRESETS=${LLAMA_PRESETS:-} \
+    -e PRESET_FILE=${PRESET_FILE:-/llama/llamacpp_presets.ini} \
+    -e MODELS=${MODELS:-} \
+    -e LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-/opt/rocm/lib:/llama/bin} \
+    -e HSA_OVERRIDE_GFX_VERSION=${HSA_OVERRIDE_GFX_VERSION:-11.5.1} \
+    -e GGML_CUDA_ENABLE_UNIFIED_MEMORY=${GGML_CUDA_ENABLE_UNIFIED_MEMORY:-1} \
+    -e GGML_HIP_FORCE_RS_GPU=${GGML_HIP_FORCE_RS_GPU:-1} \
+    -e GGML_HIP_FORCE_KV_GPU=${GGML_HIP_FORCE_KV_GPU:-1} \
+    -e GGML_HIP_ALLOC_GRAPH_RESERVE=${GGML_HIP_ALLOC_GRAPH_RESERVE:-2048} \
+    -e HSA_FORCE_FINE_GRAIN_PCIE=${HSA_FORCE_FINE_GRAIN_PCIE:-1} \
+    -e HSA_ENABLE_SDMA=${HSA_ENABLE_SDMA:-0} \
     $DOCKER_OPTS \
         $LLAMA_DOCKER_IMAGE \
             "$@"
